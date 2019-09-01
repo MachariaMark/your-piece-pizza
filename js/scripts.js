6cdbcd93ad5374e0b70b338e79cb2c6a1e67b4ff
MachariaMark <official1offmark@gmail.com>
2019-09-01 20:24:21 +0300

    added menu price section

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.parent-container').magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image',
-        //galery
+        //gallery
         gallery : {enabled: true}
         // other options
     });
